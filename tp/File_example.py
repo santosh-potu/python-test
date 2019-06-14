28743e15917cadab15de10618778b2176144513b
santosh <santoshreddy.potu@gmail.com>
2019-06-14 07:20:55 +0530

    class

---
+++ tp/File_example.py
@@ -0,0 +1,18 @@
+
+#open a file
+fo = open('foo.txt',"r+")
+
+str = fo.read(10)
+print(str)
+
+position = fo.tell()
+print ("Current file position",position)
+
+# Reposition pointer at the beginning once again
+position = fo.seek(0, 0);
+str = fo.read(10);
+print ("Again read String is : ", str)
+# Close opend file
+fo.close()
+
+
